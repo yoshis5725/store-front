5f251c2d159e3e0ec4f63ff405b096ab0b90b1d1
yoshis5725 <106788546+yoshis5725@users.noreply.github.com>
2024-06-16 23:52:06 -0600

    Scaffolded the pages

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FavoritesPage = () => {
+    return (
+        <div>
+            Favorite page
+        </div>
+    );
+};
+
+export default FavoritesPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+      <main>
+        home page
+      </main>
+  );
+};
+
+export default HomePage;
