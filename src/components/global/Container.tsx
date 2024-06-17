f99327ae016178c4edcaefee6ddea1b5b2ca13ff
yoshis5725 <106788546+yoshis5725@users.noreply.github.com>
2024-06-17 00:37:55 -0600

    Scaffolded the navbar

---
+++ src/components/global/Container.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {cn} from "@/lib/utils";
+
+const Container = ({children, className} : {children: React.ReactNode; className?: string}) => {
+    return (
+        <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+            {children}
+        </div>
+    );
+};
+
+export default Container;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Container from "@/components/global/Container";
+import Logo from "@/components/navbar/Logo";
+import NavSearch from "@/components/navbar/NavSearch";
+import CartButton from "@/components/navbar/CartButton";
+import DarkMode from "@/components/navbar/DarkMode";
+import LinksDropdown from "@/components/navbar/LinksDropdown";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className={'border-b'}>
+            <Container className={'flex flex-col sm:flex-row sm:justify-between gap-4 sm:items-center flex-wrap py-8'}>
+                <Logo />
+                <NavSearch />
+                <div className={'flex gap-4 items-center'}>
+                    <CartButton />
+                    <DarkMode />
+                    <LinksDropdown />
+                </div>
+            </Container>
+        </nav>
     );
 };
 
