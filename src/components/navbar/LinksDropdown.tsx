89039ca57e2da1ed438a5cfd161487259cf620df
yoshis5725 <106788546+yoshis5725@users.noreply.github.com>
2024-06-17 13:03:51 -0600

    Added a Prisma client connection to Supabase

--- src/components/navbar/LinksDropdown.tsx
+++ src/components/navbar/LinksDropdown.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {Button} from "@/components/ui/button";
+import {LuAlignLeft} from "react-icons/lu";
+import {links} from "@/utils/links";
+import Link from "next/link";
+
 
 const LinksDropdown = () => {
     return (
-        <div>
-            LinksDropdown
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant={'outline'} className={'flex gap-4 max-w-[100px]'}>
+                    <LuAlignLeft className={'w-6 h-6'}/>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className={'w-48'} align={'start'} sideOffset={5}>
+                {
+                    links.map(link => {
+                        return (
+                            <DropdownMenuItem key={link.href}>
+                                <Link href={link.href} className={'capitalize w-full'}>{link.label}</Link>
+                            </DropdownMenuItem>
+                        )
+                    })
+                }
+            </DropdownMenuContent>
+        </DropdownMenu>
+
     );
 };
 
---
+++ src/utils/links.ts
@@ -0,0 +1,15 @@
+type NavLink = {
+    label: string
+    href: string
+}
+
+
+export const links: NavLink[] = [
+    {label: 'home', href: '/'},
+    {label: 'shop', href: '/shop'},
+    {label: 'about', href: '/about'},
+    {label: 'products', href: '/products'},
+    {label: 'favorites', href: '/favorites'},
+    {label: 'cart', href: '/cart'},
+    {label: 'orders', href: '/orders'}
+]
