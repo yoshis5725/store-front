c37d2b89c59a712184ef7feb9821be169596d73b
yoshis5725 <106788546+yoshis5725@users.noreply.github.com>
2024-06-17 07:13:30 -0600

    Enabled toggle mode (dark, light, system) from Shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Container from "@/components/global/Container";
+import Providers from "@/app/providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "My React Store Front",
-  description: "This is a store front built with React.",
+    title: "My React Store Front",
+    description: "This is a store front built with React.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-          <body className={inter.className}>
-              <Navbar />
-              <Container className={'py-20'}>
-                  {children}
-              </Container>
-          </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <Providers>
+            <Navbar/>
+            <Container className={'py-20'}>
+                {children}
+            </Container>
+        </Providers>
+        </body>
+        </html>
+    );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Container from "@/components/global/Container";
 import Logo from "@/components/navbar/Logo";
 import NavSearch from "@/components/navbar/NavSearch";
 import CartButton from "@/components/navbar/CartButton";
-import DarkMode from "@/components/navbar/DarkMode";
+import {ModeToggle} from "@/components/navbar/DarkMode";
 import LinksDropdown from "@/components/navbar/LinksDropdown";
 
 const Navbar = () => {
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <NavSearch />
                 <div className={'flex gap-4 items-center'}>
                     <CartButton />
-                    <DarkMode />
+                    <ModeToggle />
                     <LinksDropdown />
                 </div>
             </Container>
