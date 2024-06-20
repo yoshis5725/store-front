0791a81ff954803d504075a190efeaf2860dbdb3
yoshis5725 <106788546+yoshis5725@users.noreply.github.com>
2024-06-20 05:55:19 -0600

    Created the Card carousel

--- src/components/home/HeroCarousel.tsx
+++ src/components/home/HeroCarousel.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import hero1 from '/public/images/hero1.jpg';
+import hero2 from '/public/images/hero2.jpg';
+import hero3 from '/public/images/hero3.jpg';
+import hero4 from '/public/images/hero4.jpg';
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
+import {Card, CardContent} from "@/components/ui/card";
+import Image from "next/image";
+
+const carouselItems = [hero1, hero2, hero3, hero4];
 
 const HeroCarousel = () => {
     return (
-        <div>
-            <h1>Hero Carousel</h1>
+        <div className={'hidden lg:block'}>
+            <Carousel>
+                <CarouselContent>
+                    {
+                        carouselItems.map((item, index) => {
+                            return (
+                                <CarouselItem key={index}>
+                                    <Card>
+                                        <CardContent className={'p2'}>
+                                            <Image src={item} alt={'hero'} className={'w-full h-[24rem] rounded-md object-cover'} />
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                            )}
+                        )
+                    }
+                </CarouselContent>
+                <CarouselPrevious></CarouselPrevious>
+                <CarouselNext></CarouselNext>
+            </Carousel>
         </div>
     );
 };
