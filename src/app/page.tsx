eebbfeac7635a36844345b15bbdc1ed678103fdf
yoshis5725 <106788546+yoshis5725@users.noreply.github.com>
2024-06-20 06:29:17 -0600

    Created a suspense container for the products on the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Button} from "@/components/ui/button";
 import Hero from "@/components/home/Hero";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
+import {Suspense} from "react";
+import LoadingContainer from "@/components/global/LoadingContainer";
 
 
 const HomePage = () => {
     return (
         <>
-            <Hero />
-            <FeaturedProducts />
+            <Hero/>
+            <Suspense fallback={<LoadingContainer />}>
+                <FeaturedProducts/>
+            </Suspense>
         </>
     );
 };
--- src/components/global/LoadingContainer.tsx
+++ src/components/global/LoadingContainer.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {Card, CardContent} from "@/components/ui/card";
+import {Skeleton} from "@/components/ui/skeleton";
 
 const LoadingContainer = () => {
     return (
-        <div>
-            <h1>Loading...</h1>
+        <div className={'pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'}>
+            <LoadingProduct />
+            <LoadingProduct />
+            <LoadingProduct />
+            <LoadingProduct />
+            <LoadingProduct />
+            <LoadingProduct />
         </div>
     );
 };
+
+
+function LoadingProduct() {
+    return (
+        <Card>
+            <CardContent className={'p-4'}>
+                <Skeleton className={'h-48 w-full'} />
+                <Skeleton className={'h-4 w-3/4 mt-4'} />
+                <Skeleton className={'h-4 w-1/2 mt-4'} />
+            </CardContent>
+        </Card>
+    )
+}
 
 export default LoadingContainer;
